4b3bf24a34e2f47d8108947c98a5e9bc63488aeb
99sami <s3851083@student.rmit.edu.au>
2026-03-05 16:20:08 +1100

    added stage and circle

---
+++ main.js
@@ -0,0 +1,14 @@
+// create the konca stage
+const stage = new Konva.Stage({
+  container: "konva-stage",
+  width: 500,
+  height: 500,
+});
+
+const firstlayer = new Konva.Layer();
+const circle = new Konva.Circle({
+  x: 250,
+  y: 250,
+  radius: 50,
+  fill: "red",
+});
